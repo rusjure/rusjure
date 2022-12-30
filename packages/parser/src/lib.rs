ba56b26fa573b056686ae41b58d9b69feb584797
Richard Tichý <57506608+ArtifyCZ@users.noreply.github.com>
2022-12-30 19:01:25 +0100

    Removed dummy code from parser

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -1,17 +1,3 @@
 mod expr;
 mod parser;
 mod term;
-
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-}
