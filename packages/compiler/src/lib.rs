b5cc3707cbdf0c5969f62586765d3012280e49be
Richard Tichý <57506608+ArtifyCZ@users.noreply.github.com>
2023-01-03 23:32:35 +0100

    Added LLVM calling println

--- packages/compiler/src/lib.rs
+++ packages/compiler/src/lib.rs
@@ -1,7 +1,12 @@
+use std::ffi::{c_char, CStr};
 use inkwell::context::Context;
 use inkwell::execution_engine::ExecutionEngine;
 use inkwell::module::Module;
-use rusjure_ast::Program;
+use rusjure_lst::Expression;
+
+extern "C" {
+    fn puts(s: *const c_char);
+}
 
 extern fn std_addition(x: i32, y: i32) -> i32 {
     let res = x + y;
@@ -9,41 +14,44 @@ extern fn std_addition(x: i32, y: i32) -> i32 {
     res
 }
 
+extern fn std_println(str: *const c_char) {
+    unsafe {
+        puts(str);
+        println!();
+    }
+}
+
 pub fn build_std(context: &Context, engine: &ExecutionEngine) {
     let module = context.create_module("std");
 
+    let void_t = context.void_type();
     let i32_t = context.i32_type();
-    let fn_t = i32_t.fn_type(&[i32_t.into(), i32_t.into()], false);
 
-    let fn_val = module.add_function("+", fn_t, None);
+    let println_fn_t = void_t.fn_type(&[])
+    let println_fn_v = module.add_function("println", println_fn_t, None);
 
     engine.add_module(&module).expect("TODO: panic message");
     engine.add_global_mapping(&fn_val, std_addition as usize);
 }
 
-pub fn build_module(context: &Context, _code: Program) -> Module {
+pub fn build_module<'ctx>(context: &'ctx Context, _code: Expression) -> Module<'ctx> {
     let module_name = "addition";
 
     let module = context.create_module(module_name);
 
-    let i32_type = context.i32_type();
-    let fn_type = i32_type.fn_type(&[i32_type.into(), i32_type.into()], false);
+    let void_t = context.void_type();
+    let fn_t = void_t.fn_type(&[], false);
 
-    let fn_val = module.add_function("add", fn_type, None);
-    let entry_basic_block = context.append_basic_block(fn_val, "entry");
+    let fn_v = module.add_function("main", fn_t, None);
+    let entry_b = context.append_basic_block(fn_v, "entry");
 
     let builder = context.create_builder();
-    builder.position_at_end(entry_basic_block);
+    builder.position_at_end(entry_b);
 
-    let x = fn_val.get_nth_param(0).unwrap().into_int_value();
-    let y = fn_val.get_nth_param(1).unwrap().into_int_value();
+    let str = context.const_string(b"Hello world!", true);
 
-    let add_t = i32_type.fn_type(&[i32_type.into(), i32_type.into()], false);
-    module.add_function("+", add_t, None);
-
-    let ret = builder.build_call(module.get_function("+").unwrap(), &[x.into(), y.into()], "addition")
-        .try_as_basic_value().unwrap_left();
-    builder.build_return(Some(&ret));
+    let _ = builder.build_call(module.get_function("println").unwrap(), &[str.into()], "println");
+    builder.build_return(None);
 
     module
 }
