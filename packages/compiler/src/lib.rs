79ed593a2f76954295d97e9d3a10004505d19642
Artify <57506608+ArtifyCZ@users.noreply.github.com>
2023-01-23 18:13:14 +0000

    Added failing code

--- packages/compiler/src/lib.rs
+++ packages/compiler/src/lib.rs
@@ -1,12 +1,9 @@
-use std::ffi::{c_char, CStr};
+use std::ffi::{c_char, CStr, CString};
+use inkwell::AddressSpace;
 use inkwell::context::Context;
 use inkwell::execution_engine::ExecutionEngine;
 use inkwell::module::Module;
 use rusjure_lst::Expression;
-
-extern "C" {
-    fn puts(s: *const c_char);
-}
 
 extern fn std_addition(x: i32, y: i32) -> i32 {
     let res = x + y;
@@ -14,31 +11,37 @@ extern fn std_addition(x: i32, y: i32) -> i32 {
     res
 }
 
-extern fn std_println(str: *const c_char) {
+unsafe extern fn std_println(str: *mut i8) {
     unsafe {
-        puts(str);
+        println!("Foo");
+        let str = CStr::from_ptr(str);
+        println!("Bar");
+        let str = str.to_str().unwrap();
+        println!("Baz");
+        println!("{}", str);
+        println!("Xd");
         println!();
     }
 }
 
-pub fn build_std(context: &Context, engine: &ExecutionEngine) {
-    let module = context.create_module("std");
-
+pub fn std_headers<'ctx>(module: &Module<'ctx>, context: &'ctx Context) {
     let void_t = context.void_type();
     let i32_t = context.i32_type();
 
-    let println_fn_t = void_t.fn_type(&[])
-    let println_fn_v = module.add_function("println", println_fn_t, None);
+    let i8_t = context.i8_type();
 
-    engine.add_module(&module).expect("TODO: panic message");
-    engine.add_global_mapping(&fn_val, std_addition as usize);
+    let ptr_i8_t = i8_t.ptr_type(AddressSpace::default());
+
+    let println_fn_t = void_t.fn_type(&[ptr_i8_t.into()], false);
+    let _ = module.add_function("println", println_fn_t, None);
 }
 
-pub fn build_module<'ctx>(context: &'ctx Context, _code: Expression) -> Module<'ctx> {
-    let module_name = "addition";
+pub fn link_std<'ctx>(module: &Module<'ctx>, engine: &ExecutionEngine) {
+    let println_fn_v = module.get_function("println").unwrap();
+    engine.add_global_mapping(&println_fn_v, std_println as usize);
+}
 
-    let module = context.create_module(module_name);
-
+pub fn build_module<'ctx>(module: &Module<'ctx>, context: &'ctx Context) {
     let void_t = context.void_type();
     let fn_t = void_t.fn_type(&[], false);
 
@@ -48,10 +51,8 @@ pub fn build_module<'ctx>(context: &'ctx Context, _code: Expression) -> Module<'
     let builder = context.create_builder();
     builder.position_at_end(entry_b);
 
-    let str = context.const_string(b"Hello world!", true);
+    let str = context.const_string(b"Hello world!\n", true);
 
     let _ = builder.build_call(module.get_function("println").unwrap(), &[str.into()], "println");
     builder.build_return(None);
-
-    module
 }
--- packages/compiler/src/main.rs
+++ packages/compiler/src/main.rs
@@ -1,28 +1,27 @@
 use inkwell::context::Context;
 use inkwell::execution_engine::JitFunction;
 use inkwell::OptimizationLevel;
-use rusjure_ast::{Expression, Program, Term};
-use rusjure_compiler::{build_module, build_std};
+use rusjure_compiler::{build_module, std_headers, link_std};
 
 fn main() {
-    let program: Program = vec![
-        Expression {
-            first: Box::new(Term::Symbol("println".to_string())),
-            params: vec![
-                Term::String("Hello world!".to_string())
-            ]
-        }
-    ];
+    // let program: Program = vec![
+    //     Expression {
+    //         first: Box::new(Term::Symbol("println".to_string())),
+    //         params: vec![
+    //             Term::String("Hello world!".to_string())
+    //         ]
+    //     }
+    // ];
 
     let context = Context::create();
-    let module = build_module(&context, program);
+    let module = context.create_module("main");
+    std_headers(&module, &context);
+    build_module(&module, &context);
     let engine = module.create_jit_execution_engine(OptimizationLevel::Aggressive).unwrap();
-    build_std(&context, &engine);
+    link_std(&module, &engine);
     unsafe {
-        type Addition = unsafe extern "C" fn(i32, i32) -> i32;
-        let add: JitFunction<Addition> = engine.get_function("add").unwrap();
-        let x = 512;
-        let y = 1024;
-        assert_eq!(add.call(x, y), x + y);
+        type MainFn = unsafe extern "C" fn() -> ();
+        let main: JitFunction<MainFn> = engine.get_function("main").unwrap();
+        main.call();
     }
 }
