b9bcb7c604ab67cc5bdf2f0303272ce9ab9f61e5
Richard Tichý <57506608+ArtifyCZ@users.noreply.github.com>
2023-01-26 23:06:32 +0100

    Added external standard library

--- packages/compiler/src/lib.rs
+++ packages/compiler/src/lib.rs
@@ -2,12 +2,13 @@ use std::ffi::{c_char, CStr, CString};
 use inkwell::AddressSpace;
 use inkwell::context::Context;
 use inkwell::execution_engine::ExecutionEngine;
+use inkwell::memory_buffer::MemoryBuffer;
 use inkwell::module::Module;
 use rusjure_lst::Expression;
 
 extern fn std_addition(x: i32, y: i32) -> i32 {
     let res = x + y;
-    println!("std: {} + {} = {}", x, y, res);
+    println!("stdlib: {} + {} = {}", x, y, res);
     res
 }
 
@@ -19,9 +20,14 @@ unsafe extern fn std_println(str: *const c_char) {
     }
 }
 
+pub fn std_module(context: &Context) -> Module {
+    let buffer = MemoryBuffer::create_from_memory_range(rusjure_stdlib::BITCODE, "stdlib");
+    let module = context.create_module_from_ir(buffer).unwrap();
+    module
+}
+
 pub fn std_headers<'ctx>(module: &Module<'ctx>, context: &'ctx Context) {
     let void_t = context.void_type();
-    let i32_t = context.i32_type();
 
     let i8_t = context.i8_type();
 
--- packages/compiler/src/main.rs
+++ packages/compiler/src/main.rs
@@ -1,7 +1,7 @@
 use inkwell::context::Context;
 use inkwell::execution_engine::JitFunction;
 use inkwell::OptimizationLevel;
-use rusjure_compiler::{build_module, std_headers, link_std};
+use rusjure_compiler::{build_module, std_headers, link_std, std_module};
 
 fn main() {
     // let program: Program = vec![
@@ -14,11 +14,13 @@ fn main() {
     // ];
 
     let context = Context::create();
+    let std = std_module(&context);
     let module = context.create_module("main");
-    std_headers(&module, &context);
+    module.link_in_module(std).unwrap();
+    // std_headers(&module, &context);
     build_module(&module, &context);
     let engine = module.create_jit_execution_engine(OptimizationLevel::Aggressive).unwrap();
-    link_std(&module, &engine);
+    // link_std(&module, &engine);
     unsafe {
         type MainFn = unsafe extern "C" fn() -> ();
         let main: JitFunction<MainFn> = engine.get_function("main").unwrap();
