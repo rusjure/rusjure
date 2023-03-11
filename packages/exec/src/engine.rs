d95afae3a942b1bc5971a5cd7fad51b95b9b8d84
Richard Tichý <tichy@artify.zone>
2023-03-11 12:05:01 +0100

    Implement number addition execution

--- packages/exec/src/engine.rs
+++ packages/exec/src/engine.rs
@@ -1,5 +1,7 @@
+use either::{Left, Right};
 use inkwell::context::Context;
-use rusjure_ast::Term;
+use rusjure_ast::{Expression, Term};
+use crate::IntoRsjValue;
 use crate::var::RsjValue;
 
 pub struct ExecEngine {
@@ -17,12 +19,41 @@ impl ExecEngine {
 
     pub unsafe fn execute(&mut self, term: &Term) -> RsjValue {
         match term {
-            Term::Expr(_) => todo!(),
+            Term::Expr(expression) => self.invoke(expression),
             Term::Symbol(_) => todo!(),
             Term::String(_) => todo!(),
             Term::Number(n) => n.into(),
             Term::Float(x) => x.into(),
             Term::Sequence(_) => todo!(),
+        }
+    }
+
+    pub unsafe fn invoke(&mut self, expression: &Expression) -> RsjValue {
+        let Expression { first, params } = expression;
+        let target = match &**first {
+            Term::Symbol(symbol) => symbol,
+            Term::Expr(_expr) => todo!(),
+            Term::Sequence(_seq) => todo!(),
+            _ => todo!(),
+        };
+        match target.as_str() {
+            "+" => {
+                IntoRsjValue::into_rsj_value(params.iter().fold(Left(0),
+                                                                |acc, curr| match curr {
+                    Term::Expr(_) => todo!(),
+                    Term::Symbol(_) => todo!(),
+                    Term::String(_) => todo!(),
+                    Term::Number(n) => acc
+                        .map_left(|x| x + n)
+                        .map_right(|x: f64| x + (*n as f64)),
+                    Term::Float(y) => match acc {
+                        Left(x) => Right(x as f64 + y),
+                        Right(x) => Right(x + y),
+                    },
+                    Term::Sequence(_) => todo!(),
+                }))
+            },
+            _ => todo!(),
         }
     }
 }
