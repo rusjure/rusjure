31fa258fca96f2cfcfb8585f09070bc160f45018
Richard Tichý <tichy@artify.zone>
2023-03-11 12:03:41 +0100

    Rename IntoRsjValue function due to name collision

--- packages/exec/src/var.rs
+++ packages/exec/src/var.rs
@@ -6,24 +6,24 @@ pub enum RsjValue {
 }
 
 pub trait IntoRsjValue {
-    fn into(self) -> RsjValue;
+    fn into_rsj_value(self) -> RsjValue;
 }
 
 impl<T> From<T> for RsjValue where
         T: IntoRsjValue {
     fn from(x: T) -> Self {
-        IntoRsjValue::into(x)
+        IntoRsjValue::into_rsj_value(x)
     }
 }
 
 impl IntoRsjValue for i64 {
-    fn into(self) -> RsjValue {
+    fn into_rsj_value(self) -> RsjValue {
         RsjValue::Int(self)
     }
 }
 
 impl IntoRsjValue for f64 {
-    fn into(self) -> RsjValue {
+    fn into_rsj_value(self) -> RsjValue {
         RsjValue::Float(self)
     }
 }
@@ -31,17 +31,17 @@ impl IntoRsjValue for f64 {
 impl<T1, T2> IntoRsjValue for Either<T1, T2> where
         T1: IntoRsjValue,
         T2: IntoRsjValue {
-    fn into(self) -> RsjValue {
+    fn into_rsj_value(self) -> RsjValue {
         match self {
-            Either::Left(x) => x.into(),
-            Either::Right(x) => x.into(),
+            Either::Left(x) => x.into_rsj_value(),
+            Either::Right(x) => x.into_rsj_value(),
         }
     }
 }
 
 impl<T> IntoRsjValue for &T where
         T: 'static + Copy + IntoRsjValue {
-    fn into(self) -> RsjValue {
-        IntoRsjValue::into(*self)
+    fn into_rsj_value(self) -> RsjValue {
+        IntoRsjValue::into_rsj_value(*self)
     }
 }
