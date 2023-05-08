b77e291a1f74be9da07dfbcbcd38e6c6a7500815
Richard Tichý <tichy@artify.zone>
2023-05-08 13:14:32 +0200

    Add logging

--- apps/compiler/src/main.rs
+++ apps/compiler/src/main.rs
@@ -1,2 +1,12 @@
-fn main() {
+use log::info;
+
+fn main() -> anyhow::Result<()> {
+    env_logger::builder()
+    .filter_level(log::LevelFilter::Info)
+    .parse_default_env()
+    .try_init()?;
+
+    info!("Starting up...");
+
+    Ok(())
 }
