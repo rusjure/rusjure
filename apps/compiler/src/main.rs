871b547425539b2836a83450daaac79536b16d75
Richard Tichý <tichy@artify.zone>
2023-05-08 13:54:20 +0200

    Add args parsing

--- apps/compiler/src/main.rs
+++ apps/compiler/src/main.rs
@@ -1,10 +1,16 @@
+use clap::Parser;
 use log::info;
+
+#[derive(Parser, Debug)]
+struct Args {}
 
 fn main() -> anyhow::Result<()> {
     env_logger::builder()
-    .filter_level(log::LevelFilter::Info)
-    .parse_default_env()
-    .try_init()?;
+        .filter_level(log::LevelFilter::Info)
+        .parse_default_env()
+        .try_init()?;
+
+    let _args: Args = Args::parse();
 
     info!("Starting up...");
 
